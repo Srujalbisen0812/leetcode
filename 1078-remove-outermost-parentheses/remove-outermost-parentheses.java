167af294da3ab179a5e121a44eedfcccd208fa54
Srujal Bisen <srujalbisen08@gmail.com>
2025-08-08 00:42:03 +0530

    Time: 2 ms (99.18%) | Memory: 42.7 MB (22.94%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,23 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int balance = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (balance > 0) {
+                    result.append(c);
+                }
+                balance++;
+            } else { // c == ')'
+                balance--;
+                if (balance > 0) {
+                    result.append(c);
+                }
+            }
+        }
+        return result.toString();
+    }
+}
